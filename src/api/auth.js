22e934b8125fc90d6130f32d8df53253066b990d
aobn <aobn>
2025-08-31 20:44:29 +0800

    22

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // 创建axios实例
 const api = axios.create({
   baseURL: import.meta.env.PROD 
-    ? 'https://db.webdom.0345678.xyz' 
+    ? 'https://db.goxi.top' 
     : (import.meta.env.VITE_API_BASE_URL || ''), 
   timeout: 10000,
   headers: {
